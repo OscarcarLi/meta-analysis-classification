8cb182278b2d76f4c6fc4064fad0fbe2afd9acba
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-05-13 07:07:09 +0000

    Add sans modulation config

--- train_impregmaml_miniImagenet.sh
+++ train_impregmaml_miniImagenet.sh
@@ -4,25 +4,25 @@ python main.py \
 --embedding-hidden-size 256 \
 --no-rnn-aggregation True \
 --slow-lr 0.001 \
---model-grad-clip 2.0 \
+--model-grad-clip 0. \
 --dataset miniimagenet \
---num-batches-meta-train 2 \
---num-batches-meta-val 0 \
+--num-batches-meta-train 60000 \
+--num-batches-meta-val 100 \
 --meta-batch-size 10 \
 --num-classes-per-batch 5 \
 --num-train-samples-per-class 1 \
 --num-val-samples-per-class-meta-train 15 \
 --num-val-samples-per-class-meta-val 15 \
 --img-side-len 84 \
---output-folder impregmaml_minim_5w1s_sans_modulation \
+--output-folder impregmaml_minim_5w1s_sans_modulation_10 \
 --device cuda \
---device-number 1 \
+--device-number 2 \
 --log-interval 50 \
 --save-interval 1000 \
 --val-interval 1000 \
 --num-channels 64 \
 --original-conv \
---l2-inner-loop 2.0 \
+--l2-inner-loop 10.0 \
 --hessian-inverse True \
 --no-modulation True
 
