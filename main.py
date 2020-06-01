Fix scaling during backward. Add optimizer option

main.py
@@ -521,12 +521,18 @@ def main(args):
     print(embedding_model)
     optimizers = None
     if embedding_model is None:
-        optimizers = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9, nesterov=True, weight_decay=5e-4)
+        if args.optimizer == 'adam':
+             optimizers = torch.optim.Adam(model.parameters(), lr=args.slow_lr, weight_decay=5e-4)
+        else:
+            optimizers = torch.optim.SGD(model.parameters(), lr=args.slow_lr, momentum=0.9, nesterov=True, weight_decay=5e-4)
     else:
         optimizer_specs = \
             [{'params': model.parameters()},
              {'params': embedding_model.parameters()}]
-        optimizers = torch.optim.SGD(optimizer_specs, lr=0.1, momentum=0.9, nesterov=True, weight_decay=5e-4)
+        if args.optimizer == 'adam':
+             optimizers = torch.optim.Adam(optimizer_specs, lr=args.slow_lr, weight_decay=5e-4)
+        else:
+            optimizers = torch.optim.SGD(optimizer_specs, lr=args.slow_lr, momentum=0.9, nesterov=True, weight_decay=5e-4)
 
     if args.checkpoint != '':
         print(f"loading from {args.checkpoint}")
@@ -661,10 +667,15 @@ def main(args):
     if is_training:
         # create train iterators
         train_iterator = iter(dataset['train']) 
-        lambda_epoch = lambda e: 1.0 if e < 20 else (0.06 if e < 40 else 0.012 if e < 50 else (0.0024))
+        if args.optimizer == 'sgd':
+            lambda_epoch = lambda e: 1.0 if e < 20 else (0.06 if e < 40 else 0.012 if e < 50 else (0.0024))
+        else:
+            lambda_epoch = lambda e: 1.0 if e < 20 else (0.1 if e < 40 else 0.01 if e < 50 else (0.002))
         lr_scheduler = torch.optim.lr_scheduler.LambdaLR(optimizers, lr_lambda=lambda_epoch, last_epoch=-1)
         for iter_start in range(1, num_batches['train'], args.val_interval):
             lr_scheduler.step()
+            for param_group in optimizers.param_groups:
+                print('optimizer:', args.optimizer, 'lr:', param_group['lr'])
             try:
                 train_result = trainer.run(train_iterator, is_training=True, 
                     start=iter_start, stop=iter_start+args.val_interval)
@@ -877,6 +888,8 @@ if __name__ == '__main__':
         help='name of the output folder')
     parser.add_argument('--device', type=str, default='cuda',
         help='set the device (cpu or cuda)')
+    parser.add_argument('--optimizer', type=str, default='adam',
+        help='optimizer')
     parser.add_argument('--device-number', type=str, default='0',
                         help='gpu device number')
     parser.add_argument('--num-workers', type=int, default=4,

maml/algorithm_trainer.py
@@ -315,6 +315,12 @@ class Implicit_Gradient_based_algorithm_trainer(object):
 
 
     def run(self, dataset_iterator, is_training=False, meta_val=False, start=1, stop=1):
+
+        if is_training:
+            self._algorithm._model.train()
+        else:
+            self._algorithm._model.eval()
+
         # looping through the entire meta_dataset once
         sum_train_measurements_trajectory_over_meta_set = defaultdict(float)
         # sum_test_measurements_before_adapt_over_meta_set = defaultdict(float)
@@ -356,7 +362,7 @@ class Implicit_Gradient_based_algorithm_trainer(object):
                     with torch.no_grad():
                         features_test = self._algorithm._model(batch=test_task.x, modulation=modulation_train)
 
-                test_pred_after_adapt = F.linear(features_test, weight=adapted_params)
+                test_pred_after_adapt = self._algorithm._model.scale * F.linear(features_test, weight=adapted_params)
                 test_loss_after_adapt = self._outer_loss_func(test_pred_after_adapt, test_task.y)
                 
                 test_measurements_after_adapt_over_batch['loss'].append(test_loss_after_adapt.item())
@@ -371,7 +377,8 @@ class Implicit_Gradient_based_algorithm_trainer(object):
                     X_test = features_test.detach().cpu().numpy()
                     y_test = (test_task.y).cpu().numpy()
                     w = adapted_params.detach().cpu().numpy()
-                    test_grad_w = logistic_regression_grad_with_respect_to_w(X_test, y_test, w)
+                    test_grad_w = logistic_regression_grad_with_respect_to_w(X_test, y_test,
+                        self._algorithm._model.scale.detach().cpu().numpy() * w)
 
                     # train_hessian_inverse = np.linalg.inv(train_hessian)
                     # train_hessian_inv_test_grad = np.matmul(train_hessian_inverse, test_grad_w)
@@ -563,6 +570,12 @@ class Metaoptnet_algorithm_trainer(object):
         
 
     def run(self, dataset_iterator, is_training=False, meta_val=False, start=1, stop=1):
+
+        if is_training:
+            self._algorithm._model.train()
+        else:
+            self._algorithm._model.eval()
+
         # looping through the entire meta_dataset once
         sum_train_measurements_trajectory_over_meta_set = defaultdict(float)
         # sum_test_measurements_before_adapt_over_meta_set = defaultdict(float)