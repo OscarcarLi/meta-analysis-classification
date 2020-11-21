Fix meta-gradient computation for MAML

algorithm_trainer/algorithms/algorithm.py
@@ -93,6 +93,12 @@ class InitBasedAlgorithm(Algorithm):
         for param in self._model.parameters():
             self._backed_opt_state.append(torch.zeros_like(param)) 
         
+        print("--"*20+"learnable params"+"--"*20)
+        for name, param in self._model.named_parameters():
+            if param.requires_grad:
+                print(name)
+        
+        
 
         
 
@@ -104,7 +110,7 @@ class InitBasedAlgorithm(Algorithm):
         return logits
 
 
-    def compute_gradient_wrt_model(self, X, y, model, create_graph):
+    def compute_gradient_wrt_model(self, X, y, model, params_wrt_grad_is_computed, create_graph):
         """Compute gradient of self._loss_func(X, y; model),
         based on support, support_labels set but with respect to parameters in model
         """
@@ -118,7 +124,7 @@ class InitBasedAlgorithm(Algorithm):
         # for n, p in self._model.named_parameters():
         #     print(n, torch.norm(p))
         accu = accuracy(logits, y)
-        grad_list = torch.autograd.grad(loss, model.parameters(),
+        grad_list = torch.autograd.grad(loss, params_wrt_grad_is_computed,
                                     create_graph=create_graph, allow_unused=False, only_inputs=True)
         # allow_unused If False, specifying inputs that were not used when computing outputs
         # (and therefore their grad is always zero) is an error. Defaults to False.
@@ -152,6 +158,7 @@ class InitBasedAlgorithm(Algorithm):
     def perform_update(self, grad_list):
 
         if self._inner_update_method == 'sgd':
+            self._time_step += 1
             return grad_list
 
         elif self._inner_update_method == 'adam':
@@ -184,6 +191,8 @@ class InitBasedAlgorithm(Algorithm):
         do_not_copy = [tensor for tensor in chain(model.parameters(), model.buffers())
                 if tensor not in updates]
         # print("do_not_copy", do_not_copy)
+        # for name, _ in model.named_buffers():
+        #     print(name)
         return copy_and_replace(model, updates, do_not_copy=do_not_copy)
                 
 
@@ -217,8 +226,11 @@ class InitBasedAlgorithm(Algorithm):
             # if param.grad is None:
             #     param.grad = torch.zeros_like(param)  
             if param.grad is not None: 
+                # print(f"Time step {self._time_step} {param.name} prev {torch.norm(param.grad)}")
                 param.grad += calculated_grad.detach()
+                # print(f"Time step {self._time_step} {param.name} curr {torch.norm(param.grad)}")
             else:
+                # print(f"Time step {self._time_step} No grad present for name")
                 param.grad = calculated_grad.detach()
 
 
@@ -240,7 +252,7 @@ class InitBasedAlgorithm(Algorithm):
         assert self._num_updates > 0
         for i in range(self._num_updates):
             support_loss, support_accu, grad_list = self.compute_gradient_wrt_model(
-                X=support, y=support_labels, model=updated_model,
+                X=support, y=support_labels, model=updated_model, params_wrt_grad_is_computed=updated_model.parameters(),
                 create_graph=self._second_order)
             updated_model = self.get_updated_model(model=updated_model, 
                 grad_list=grad_list)
@@ -256,15 +268,15 @@ class InitBasedAlgorithm(Algorithm):
         # Now compute loss on query set and from that the outer gradient
         if self._method == 'MAML':
             query_loss, query_accu, outer_grad_list = self.compute_gradient_wrt_model(
-                X=query, y=query_labels, model=self._model,
+                X=query, y=query_labels, model=updated_model, params_wrt_grad_is_computed=self._model.parameters(),
                 create_graph=False)
         elif self._method == 'FOMAML':
             query_loss, query_accu, outer_grad_list = self.compute_gradient_wrt_model(
-                X=query, y=query_labels, model=updated_model,
+                X=query, y=query_labels, model=updated_model, params_wrt_grad_is_computed=updated_model.parameters(),
                 create_graph=False)
         elif self._method == 'Reptile': 
             query_loss, query_accu, grad_list = self.compute_gradient_wrt_model(
-                X=query, y=query_labels, model=updated_model,
+                X=query, y=query_labels, model=updated_model, params_wrt_grad_is_computed=updated_model.parameters(),
                 create_graph=False)
             updated_model = self.get_updated_model(model=updated_model, 
                 grad_list=grad_list)

algorithm_trainer/models/conv32.py
@@ -116,24 +116,10 @@ def conv_block(in_channels, out_channels):
     )
 
 
-class Convnet(nn.Module):
-
-    def __init__(self, x_dim=3, hid_dim=64, z_dim=64):
-        super().__init__()
-        self.encoder = nn.Sequential(
-            conv_block(x_dim, hid_dim),
-            conv_block(hid_dim, hid_dim),
-            conv_block(hid_dim, hid_dim),
-            conv_block(hid_dim, z_dim),
-        )
-        self.out_channels = 1600
-
-
-
-class Conv64(nn.Module):
-    def __init__(self, num_classes, classifier_type='avg-linear', x_dim=3, h_dim=64, z_dim=64, 
+class Conv32(nn.Module):
+    def __init__(self, num_classes, classifier_type='avg-linear', x_dim=3, h_dim=32, z_dim=32, 
         retain_last_activation=True, activation='ReLU', add_bias=False, projection=False, classifier_metric='euclidean', lambd=0.):
-        super(Conv64, self).__init__()
+        super(Conv32, self).__init__()
         
         self.encoder = nn.Sequential(
             conv_block(x_dim, h_dim),
@@ -159,8 +145,8 @@ class Conv64(nn.Module):
         # classifier creation
         self.projection = projection
         print("Unit norm projection is ", self.projection)
-        print("Avg pool is always False for Conv64")
-        self.final_feat_dim = 1600
+        print("Avg pool is always False for Conv32")
+        self.final_feat_dim = 800
         self.num_classes = num_classes
         self.no_fc_layer = (classifier_type == "no-classifier")
         self.classifier_type = classifier_type
@@ -180,7 +166,19 @@ class Conv64(nn.Module):
         self.add_bias = add_bias
         # self.scale_factor = nn.Parameter(torch.FloatTensor([10.0]))
         # self.scale_factor = 1.
+
+        # initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                print("Initializing Conv2d with Xavier Uniform")
+                nn.init.xavier_uniform_(m.weight)
+            elif isinstance(m, nn.Linear):
+                print("Initializing Linear with Xavier Uniform")
+                nn.init.xavier_uniform_(m.weight)
+
+        self.running_stats = {} 
         
+                
 
     def forward(self, x, features_only=True):
         x = self.encoder(x)
@@ -199,4 +197,3 @@ class Conv64(nn.Module):
             x = self.fc.forward(x)
         
         return x
-

algorithm_trainer/models/conv48.py
@@ -8,6 +8,7 @@ import numpy as np
 import torch.nn.functional as F
 from torch.nn.utils.weight_norm import WeightNorm
 from collections import defaultdict
+from copy import copy
 
 
 
@@ -84,7 +85,7 @@ class ConvBlock(nn.Module):
         
         self.block = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels)
+            nn.BatchNorm2d(out_channels, track_running_stats=False)
         )
         
         if retain_activation:
@@ -106,7 +107,7 @@ class ConvBlock(nn.Module):
 
 
 def conv_block(in_channels, out_channels):
-    bn = nn.BatchNorm2d(out_channels)
+    bn = nn.BatchNorm2d(out_channels, track_running_stats=False)
     nn.init.uniform_(bn.weight) # for pytorch 1.2 or later
     return nn.Sequential(
         nn.Conv2d(in_channels, out_channels, 3, padding=1),
@@ -132,7 +133,8 @@ class Convnet(nn.Module):
 
 class Conv48(nn.Module):
     def __init__(self, num_classes, classifier_type='avg-linear', x_dim=3, h_dim=48, z_dim=48, 
-        retain_last_activation=True, activation='ReLU', add_bias=False, projection=False, classifier_metric='euclidean', lambd=0.):
+        retain_last_activation=True, activation='ReLU', add_bias=False, projection=False, classifier_metric='euclidean', lambd=0.,
+        device='cuda'):
         super(Conv48, self).__init__()
         
         self.encoder = nn.Sequential(
@@ -180,6 +182,21 @@ class Conv48(nn.Module):
         self.add_bias = add_bias
         # self.scale_factor = nn.Parameter(torch.FloatTensor([10.0]))
         # self.scale_factor = 1.
+
+        
+
+        # initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                print("Initializing Conv2d with Xavier Uniform")
+                nn.init.xavier_uniform_(m.weight)
+            elif isinstance(m, nn.Linear):
+                print("Initializing Linear with Xavier Uniform")
+                nn.init.xavier_uniform_(m.weight)
+
+        self.running_stats = {} 
+        # self.backup_running_statistics()
+        self.device=device
         
 
     def forward(self, x, features_only=True):
@@ -199,3 +216,47 @@ class Conv48(nn.Module):
             x = self.fc.forward(x)
         
         return x
+
+
+
+    # def backup_running_statistics(self):
+        
+    #     for name, module in self.named_modules():
+    #         if isinstance(module, nn.BatchNorm2d):
+    #             print(name, module.running_mean)
+            
+    #             self.running_stats[f"{name}.running_mean"] = copy(module.running_mean.data)
+    #             self.running_stats[f"{name}.running_var"] = copy(module.running_var.data)
+    #     print(self.running_stats)
+
+
+    # def restore_running_statistics(self):
+        
+    #     for name, module in self.named_modules():
+    #         if isinstance(module, nn.BatchNorm2d):
+    #             module.running_mean = nn.Parameter(
+    #                 self.running_stats[f"{name}.running_mean"].to(device=self.device), requires_grad=False)
+    #             module.running_var = nn.Parameter(
+    #                 self.running_stats[f"{name}.running_var"].to(device=self.device), requires_grad=False)
+    #             print(name, module)
+
+    # def restore_running_statistics(self):
+
+    #                 self.backup_running_mean.data = copy(self.running_mean.data)
+    #         self.backup_running_var.data = copy(self.running_var.data)
+
+    #     momentum = self.momentum
+
+    #     output = F.batch_norm(input, running_mean, running_var, weight, bias,
+    #                           training=True, momentum=momentum, eps=self.eps)
+
+    #     return output
+
+    # def restore_backup_stats(self):
+    #     """
+    #     Resets batch statistics to their backup values which are collected after each forward pass.
+    #     """
+    #     if self.use_per_step_bn_statistics:
+    #         self.running_mean = nn.Parameter(self.backup_running_mean.to(device=self.device), requires_grad=False)
+    #         self.running_var = nn.Parameter(self.backup_running_var.to(device=self.device), requires_grad=False)
+    #     pass

data_layer/datasets.py
@@ -3,6 +3,8 @@ from PIL import Image
 import json
 import numpy as np
 import torchvision.transforms as transforms
+import concurrent.futures
+import tqdm
 import os
 identity = lambda x:x
 
@@ -52,11 +54,15 @@ class MetaDataset:
         self.cl_list = np.unique(self.meta['image_labels']).tolist()
 
         self.sub_meta = {}
+        self.class_images = {}
         for cl in self.cl_list:
             self.sub_meta[cl] = []
 
         for x,y in zip(self.meta['image_names'],self.meta['image_labels']):
             self.sub_meta[y].append(x)
+            
+        for cl in self.cl_list:
+            self.class_images[cl] = ClassImages(self.sub_meta[cl], cl)
 
         if n_shot > 0:
             self.support_sub_dataloader = [] 
@@ -65,9 +71,9 @@ class MetaDataset:
                                     num_workers = 0, #use main thread only or may receive multiple batches
                                     pin_memory = False)        
             for cl in self.cl_list:
-                sub_dataset = SubMetaDataset(
-                    (np.array(self.sub_meta[cl])[np.random.choice(len(self.sub_meta[cl]), self.fix_support, replace=False)]).tolist() if self.fix_support else self.sub_meta[cl], 
-                    cl, transform = support_transform)
+                print("Setting support loader for class", cl)
+                sub_dataset = SupportOrQueryForSubMetadataset(self.class_images[cl], 
+                    n_images=self.fix_support, cl=cl, transform=support_transform)
                 self.support_sub_dataloader.append(
                     torch.utils.data.DataLoader(sub_dataset, **support_sub_data_loader_params))
 
@@ -78,7 +84,8 @@ class MetaDataset:
                                     num_workers = 0, #use main thread only or may receive multiple batches
                                     pin_memory = False)        
             for cl in self.cl_list:
-                sub_dataset = SubMetaDataset(self.sub_meta[cl], cl, transform = query_transform)
+                print("Setting query loader for class", cl)
+                sub_dataset = SupportOrQueryForSubMetadataset(self.class_images[cl], cl=cl, transform = query_transform)
                 self.query_sub_dataloader.append(
                     torch.utils.data.DataLoader(sub_dataset, **query_sub_data_loader_params))
                 
@@ -101,25 +108,70 @@ class MetaDataset:
 
 
 
-class SubMetaDataset:
+def load_image(image_path):
+    img = Image.open(image_path).convert('RGB')
+    return img
 
-    def __init__(self, sub_meta, cl, 
-        transform=transforms.ToTensor(), target_transform=identity, printt=False):
 
-        self.sub_meta = sub_meta
+
+class SupportOrQueryForSubMetadataset:
+
+    def __init__(self, class_images, cl, n_images=0,
+        transform=transforms.ToTensor(), target_transform=identity):
+
+        self.class_images = class_images
+        self.indices = np.random.choice(len(class_images), n_images, replace=False)\
+             if n_images else np.arange(len(class_images))
+        print(f"using {len(self.indices)} images from class")
         self.cl = cl 
         self.transform = transform
         self.target_transform = target_transform
-        self.print=printt
         
+
     def __getitem__(self,i):
-        if self.print:
-            print( '%d -%d' %(self.cl,i), os.path.join(self.sub_meta[i]))
-        image_path = os.path.join(self.sub_meta[i])
-        img = Image.open(image_path).convert('RGB')
+        # image_path = os.path.join(self.sub_meta[i])
+        # img = Image.open(image_path).convert('RGB')
+        img = self.class_images[self.indices[i]]
         img = self.transform(img)
         target = self.target_transform(self.cl)
         return img, target
+
+    def __len__(self):
+        return len(self.indices)
+
+
+
+
+class ClassImages:
+
+    def __init__(self, sub_meta, cl, printt=False):
+
+        self.sub_meta = sub_meta
+        self.images = []
+        self.cl = cl 
+        self.print=printt
+
+
+        print("Attempt loading class {} into memory".format(cl))
+        # with tqdm.tqdm(total=len(self.sub_meta)) as pbar_memory_load:
+        with concurrent.futures.ProcessPoolExecutor(max_workers=8) as executor:
+            # Process the list of files, but split the work across the process pool to use all CPUs!
+            for image in executor.map(load_image, self.sub_meta):
+                self.images.append(image)
+                    # pbar_memory_load.update(1)
+        print("Done loading class {} into memory -- found {} images".format(cl, len(self.images)))
+                        
+    
+
+    def __getitem__(self,i):
+        # if self.print:
+        #     print( '%d -%d' %(self.cl,i), os.path.join(self.sub_meta[i]))
+        # image_path = os.path.join(self.sub_meta[i])
+        # img = Image.open(image_path).convert('RGB')
+        img = self.images[i]
+        # img = self.transform(img)
+        # target = self.target_transform(self.cl)
+        return img
 
     def __len__(self):
         return len(self.sub_meta)