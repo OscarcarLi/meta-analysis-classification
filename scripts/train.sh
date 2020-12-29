3682744b11eff72b572240f7070e8527cd71c357
ars22 <amrithsetlur22@gmail.com>
2020-12-29 07:28:17 +0000

    Add more logs and no_grad to evaluation

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-output='metal_MI_r12_PNeuc_n5s5q15Vtb4_SGD0.1Drop20'
+output='fixS1_MI_r12_PNeuc_n5s1q15Vtb4_SGD0.1det'
 # method_dataset_model_innerAlg_config_outerOpt 
-device='0,1'
+device='1'
 mkdir -p logs
 mkdir -p runs
 
 CUDA_VISIBLE_DEVICES="$device" nohup python main.py \
---fix-support 0 \
+--fix-support 1 \
 --model-type resnet_12 \
 --avg-pool True \
 --projection False \
@@ -19,12 +19,12 @@ CUDA_VISIBLE_DEVICES="$device" nohup python main.py \
 --n-epochs 60 \
 --batch-size-train 4 \
 --n-way-train 5 \
---n-shot-train 5 \
+--n-shot-train 1 \
 --n-query-train 15 \
 --n-iters-per-epoch 1000 \
 --batch-size-val 2 \
 --n-way-val 5 \
---n-shot-val 5 \
+--n-shot-val 1 \
 --n-query-val 15 \
 --n-iterations-val 250 \
 --support-aug False \
@@ -35,7 +35,7 @@ CUDA_VISIBLE_DEVICES="$device" nohup python main.py \
 --lr 0.1 \
 --weight-decay 0.0005 \
 --grad-clip 0. \
---drop-lr-epoch 20 \
+--drop-lr-epoch 20,40,50 \
 --lr-scheduler-type deterministic \
 --eps 0. \
 --restart-iter 0 \
