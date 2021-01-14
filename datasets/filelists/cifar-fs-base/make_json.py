3d2203aa7aeb1ef044c50f9fdbe62ec59ca12068
ars22 <amrithsetlur22@gmail.com>
2021-01-14 15:56:38 -0500

    add pathname

--- datasets/filelists/cifar-fs-base/make_json.py
+++ datasets/filelists/cifar-fs-base/make_json.py
@@ -63,7 +63,7 @@ count = 80
 for each in classes:
     each = each.strip()
     test['label_names'].append(each)
-    files = sorted(glob.glob( '/cifar100/data/' + each + '/*'))
+    files = sorted(glob.glob( pathname + '/cifar100/data/' + each + '/*'))
     for image_name in files:
         test['image_names'].append( image_name)
         test['image_labels'].append(count)
