Fix novel acc computation

analysis/compute_novel_acc_variance.py
@@ -175,7 +175,7 @@ def main(args):
     #                LOGGING AND SAVING                #
     ####################################################
     args.output_folder = ensure_path('./runs/{0}'.format(args.output_folder))
-    eval_results = f'{args.output_folder}/evallarge_results.txt'
+    eval_results = f'{args.output_folder}/novel_acc_variance_{args.n_chosen_classes}.txt'
     with open(eval_results, 'w') as f:
         f.write("--"*20 + "EVALUATION RESULTS" + "--"*20 + '\n')
 
@@ -226,10 +226,11 @@ def main(args):
                     args, 
                     dataset_name=dataset_name, 
                     checkpoint_path=args.checkpoint_1)
-    model_2 = create_model_and_load_chkpt(
-                    args, 
-                    dataset_name=dataset_name, 
-                    checkpoint_path=args.checkpoint_2)
+    if args.algorithm_2 != '':
+        model_2 = create_model_and_load_chkpt(
+                        args, 
+                        dataset_name=dataset_name, 
+                        checkpoint_path=args.checkpoint_2)
 
     ####################################################
     #        ALGORITHM AND ALGORITHM TRAINER           #
@@ -239,21 +240,29 @@ def main(args):
                     args, 
                     algorithm_type=args.algorithm_1,
                     model=model_1)
-    trainer_2 = create_alg_and_trainer(
-                    args, 
-                    algorithm_type=args.algorithm_2,
-                    model=model_2)
+    if args.algorithm_2 != '':
+        trainer_2 = create_alg_and_trainer(
+                        args, 
+                        algorithm_type=args.algorithm_2,
+                        model=model_2)
 
     ####################################################
     #                    EVALUATION                    #
     ####################################################
 
+    # check if there is pre specified list of novel classes for each run
+    chosen_classes_indices_list = None
+    if args.chosen_classes_indices_list != '':
+        print(f'Loading novel classes for each run from file {args.chosen_classes_indices_list}') 
+        chosen_classes_indices_list = []
+        with open(args.chosen_classes_indices_list, 'r') as f:
+            chosen_classes_indices_list = [line.strip().split(" ") for line in f.readlines()]
 
     for run in range(args.n_runs):
         
         if args.algorithm_1 == 'TransferLearning':
 
-            assert args.algorithm_2 == 'TransferLearning'
+            assert args.algorithm_2 == 'TransferLearning' or args.algorithm_2 == '' 
 
             novelval_dataset = SimpleDataset(
                             dataset_name=dataset_name,
@@ -269,18 +278,24 @@ def main(args):
 
             results_1 = trainer_1.run(
                 mt_loader=novelval_loaders, is_training=False, evaluate_supervised_classification=True)
-            results_2 = trainer_2.run(
-                mt_loader=novelval_loaders, is_training=False, evaluate_supervised_classification=True)
+            if args.algorithm_2 != '':
+                results_2 = trainer_2.run(
+                    mt_loader=novelval_loaders, is_training=False, evaluate_supervised_classification=True)
             
             with open(eval_results, 'a') as f:
                 f.write(f"Run{run+1}: ")
                 f.write(f"Alg_1: Loss {round(results_1['test_loss_after']['loss'], 3)} Acc {round(results_1['test_loss_after']['accu'], 3)} ")
-                f.write(f"Alg_2: Loss {round(results_2['test_loss_after']['loss'], 3)} Acc {round(results_2['test_loss_after']['accu'], 3)}"+"\n")            
-
+                if args.algorithm_2 != '':
+                    f.write(f"Alg_2: Loss {round(results_2['test_loss_after']['loss'], 3)} Acc {round(results_2['test_loss_after']['accu'], 3)}")            
+                f.write("\n")
 
         else:
-            chosen_classes = np.random.choice(
-                list(novelval_classes.keys()), args.n_chosen_classes,replace=False)
+            if chosen_classes_indices_list is None:
+                chosen_classes = np.random.choice(
+                    list(novelval_classes.keys()), args.n_chosen_classes,replace=False)
+            else:
+                novel_class_keys = list(novelval_classes.keys())
+                chosen_classes = [novel_class_keys[idx] for idx in chosen_classes_indices_list[run]]
             novelval_loaders = MetaDataLoader(
                     dataset=novelval_meta_datasets,
                     n_batches=args.n_iterations_val,
@@ -296,15 +311,18 @@ def main(args):
 
             results_1 = trainer_1.run(
                 mt_loader=novelval_loaders, is_training=False)
-            results_2 = trainer_2.run(
-                mt_loader=novelval_loaders, is_training=False)
+            if args.algorithm_2 != '':
+                results_2 = trainer_2.run(
+                    mt_loader=novelval_loaders, is_training=False)
         
         
             with open(eval_results, 'a') as f:
                 f.write(f"Run{run+1} {args.n_way_val}w{args.n_shot_val}s: ")
                 f.write(f"Classes {chosen_classes} ")
                 f.write(f"Alg_1: Loss {round(results_1['test_loss_after']['loss'], 3)} Acc {round(results_1['test_loss_after']['accu'], 3)} ")
-                f.write(f"Alg_2: Loss {round(results_2['test_loss_after']['loss'], 3)} Acc {round(results_2['test_loss_after']['accu'], 3)}"+"\n")            
+                if args.algorithm_2 != '':
+                    f.write(f"Alg_2: Loss {round(results_2['test_loss_after']['loss'], 3)} Acc {round(results_2['test_loss_after']['accu'], 3)}")
+                f.write("\n")
 
 
 
@@ -319,7 +337,7 @@ if __name__ == '__main__':
 
     # Algorithm
     parser.add_argument('--algorithm-1', type=str, help='type of algorithm-1')
-    parser.add_argument('--algorithm-2', type=str, help='type of algorithm-2')
+    parser.add_argument('--algorithm-2', type=str, help='type of algorithm-2', default='')
 
     # Model
     parser.add_argument('--model-type', type=str, default='gatedconv',
@@ -372,6 +390,7 @@ if __name__ == '__main__':
     
 
     # Miscellaneous
+    parser.add_argument('--chosen-indices-class-list', type=str, default='')
     parser.add_argument('--output-folder', type=str, default='maml',
         help='name of the output folder')
     parser.add_argument('--device-number', type=str, default='0',

src/data/datasets.py
@@ -519,6 +519,6 @@ class SimpleDataset(torch.utils.data.Dataset):
 if __name__ == '__main__':
 
     cis = \
-    ClassImagesSet(data_file='/home/oscarli/projects/meta-analysis-classification/data/new_miniimagenet/val.json',
+    ClassImagesSet(data_file='/home/anonymous/projects/meta-analysis-classification/data/new_miniimagenet/val.json',
                    preload=False)
     print(cis.class_images_set)