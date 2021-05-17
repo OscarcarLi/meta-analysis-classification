6e1c93a217701e46ee7530fa314bafa08adcd7f7
Oscar Li <oscarliduke@gmail.com>
2021-05-16 20:33:43 -0700

    add training scripts for anyfalse zappos and rename the training scripts for allfalse zappos

--- scripts/conv-64/zappos/train_fixsq_metal_Ridge_zappos3000allfalse_conv64_50_0.1.sh
+++ scripts/conv-64/zappos/train_fixsq_metal_Ridge_zappos3000allfalse_conv64_50_0.1.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-output='metal_zappos3000_conv64_PN_n2s10q10b1_nep100_SGD0.001Drop50_0.1'
+output='metal_zappos3000allfalse_conv64_Ridge_n2s10q10b1_nep100_SGD0.001Drop50_0.1'
 # method_dataset_model_innerAlg_config_outerOpt_misc
 device='1'
 mkdir -p logs
@@ -10,13 +10,14 @@ mkdir -p runs
 
 python fed_main_fixsq.py \
 --model-type conv64 \
---avg-pool True \
+--avg-pool False \
 --projection False \
 --num-classes-train 0 \
---algorithm ProtoNet \
---scale-factor 10. \
+--algorithm Ridge \
+--scale-factor 1. \
+--learnable-scale True \
 --classifier-metric euclidean \
---dataset-path /home/oscarli/projects/meta-analysis-classification/fed_data/zappos/zappos-minImg1000-alltrue_vs_allfalse-nsamp8000-ns10-nq10-tr1000val1000test1000 \
+--dataset-path fed_data/zappos/zappos-minImg1000-alltrue_vs_allfalse-nsamp8000-ns10-nq10-tr1000val1000test1000 \
 --img-side-len 28 \
 --n-epochs 100 \
 --batch-size-train 1 \
@@ -39,3 +40,8 @@ python fed_main_fixsq.py \
 # --support-aug False \
 # --query-aug True \
 # --randomize-query True \
+
+# Train SVM and Ridge first. 
+# increasing the batch size for SVM, PN and Ridge [from 1 to ]
+# we might need to increase the validation and testing tasks, currently it is 1000
+# optimization schedule and initial learning rate. 
